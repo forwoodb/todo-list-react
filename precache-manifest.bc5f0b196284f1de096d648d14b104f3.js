864a82a9a4b66dfa83fe8b712a7b4ce2868532de
Brent Forwood <forwoodb77@gmail.com>
2019-09-21 02:31:35 -0400

    Updates

---
+++ precache-manifest.bc5f0b196284f1de096d648d14b104f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fab2fa7ca7ccdfe57a52a7e396546cc4",
+    "url": "/todo-list-react/index.html"
+  },
+  {
+    "revision": "69a9d9d7c45703a27dce",
+    "url": "/todo-list-react/static/css/main.19d56480.chunk.css"
+  },
+  {
+    "revision": "08e72e7f05dd5b0c7721",
+    "url": "/todo-list-react/static/js/2.adf86020.chunk.js"
+  },
+  {
+    "revision": "69a9d9d7c45703a27dce",
+    "url": "/todo-list-react/static/js/main.b7305424.chunk.js"
+  },
+  {
+    "revision": "35e3544cd546257cfadf",
+    "url": "/todo-list-react/static/js/runtime~main.bb4d8c92.js"
+  }
+]);
